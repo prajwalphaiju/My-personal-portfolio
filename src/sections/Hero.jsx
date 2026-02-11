dfd1c435cfa6419b20d4cf1287cf68554a6c0037
Prajwal Phaiju <prajwalphaiju5@gmail.com>
2026-02-11 12:37:43 +1100

    create left section of the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/Button";
+import { ArrowRight, Download, Github, Linkedin } from "lucide-react";
 import backgroundImage from "@/assets/background.png";
+import { AnimatedBorderButton } from "../components/AnimatedBorderButton";
 
 export const Hero = () => {
   return (
@@ -42,15 +45,55 @@ export const Hero = () => {
             <div className="animate-fade-in">
               <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass ">
                 <span className="w-2 h-2 bg-primary rounded-full animate-pulse" />
-                Software Enginner
+                Software Engineer
               </span>
             </div>
             {/* Headline */}
-            <div>
-              <h1 className="space-y-4">
-                Turning <span className="text-primary glow text">Ideas</span>{" "}
-                into Interactive Reality.
+            <div className="space-y-4">
+              <h1 className="text-5xl md:text-5xl lg:text-6xl font-bold leading-tight animate-fade-in animation-delay-100">
+                Turning <span className="text-primary glow-text">Ideas</span>{" "}
+                into
+                <br />{" "}
+                <span className="font-serif italic font-normal text-white">
+                  Interactive Reality.
+                </span>
               </h1>
+              <p className="text-lg text-muted-foreground max-w-lg animate-fade-in animation-delay-200">
+                Hi, I’m Prajwal Phaiju — IT Graduate and Software Engineer. I
+                build production-ready applications with a strong focus on
+                stability, performance, and reliability.
+                <br />
+                <br />
+                <span className="font-serif  font-normal text-white">
+                  React, TypeScript, Node.js — engineered for real-world use.
+                </span>
+              </p>
+            </div>
+            {/* CTAs */}
+            <div className="flex flex-wrap gap-4 animate-fade-in animation-delay-300">
+              <Button size="lg">
+                Contact Me <ArrowRight className="w-5 h-5" />
+              </Button>
+
+              <AnimatedBorderButton>
+                <Download className="w-5 h-5" />
+                Download Resume
+              </AnimatedBorderButton>
+            </div>
+            {/* Social Links*/}
+            <div className="flex items-center gap-4 animate-fade-in animation-delay-400">
+              <span className="text-sm text-muted-foreground">Follow: </span>
+              {[
+                { icon: Github, href: "https://github.com/prajwalphaiju" },
+                {
+                  icon: Linkedin,
+                  href: "https://www.linkedin.com/in/prajwalphaiju/",
+                },
+              ].map((social, index) => (
+                <a key={index} href={social.href} className="p-2 rounded-full glass hover:bg-primary/10 hover:text-primary transition-all duration-300">
+                  {<social.icon  className="w-6 h-6"/>}
+                </a>
+              ))}
             </div>
           </div>
           {/* Right section - Profile Image */}
@@ -58,4 +101,4 @@ export const Hero = () => {
       </div>
     </section>
   );
-};
+}; 
